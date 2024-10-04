27997fafbcf2cf66965d5aed788b09094c41d99e
Ville Halonen <71012693+hzville@users.noreply.github.com>
2024-10-05 01:17:26 +0300

    9.10

--- patientor/backend/src/index.ts
+++ patientor/backend/src/index.ts
@@ -1,14 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import diagnosesRouter from './routes/diagnoses';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-
-app.get('/api/ping', (_req, res) => {
-  res.send('pong');
-});
+app.use('/api/diagnoses', diagnosesRouter);
 
 const PORT = 3001;
 app.listen(PORT, () => {
