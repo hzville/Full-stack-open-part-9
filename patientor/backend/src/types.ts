2041d3dabde46c2edf2e6193c7c88e81ce6ceb90
Ville Halonen <71012693+hzville@users.noreply.github.com>
2024-10-18 15:30:24 +0300

    9.23

--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -3,8 +3,58 @@ export interface Diagnosis {
   name: string;
   latin?: string;
 }
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {
+
+export enum Gender {
+  Male = "male",
+  Female = "female",
+  Other = "other"
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+export interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+export interface SickLeave{
+  startDate: string;
+  endDate: string;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+interface HospitalEntry extends BaseEntry{
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
 }
 
 export interface Patient {
@@ -20,9 +70,3 @@ export interface Patient {
 export type NonSensitivePatientData = Omit<Patient, 'ssn' | 'entries'>;
 
 export type NewPatientObject = Omit<Patient, 'id'>;
-
-export enum Gender {
-  Male = "male",
-  Female = "female",
-  Other = "other"
-}
